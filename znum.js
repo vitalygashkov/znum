3f9c4302dd5539d26e8c7153d404e63a2e07b7df
Vitaly Gashkov <vitalygashkov@vk.com>
2024-09-01 14:40:45 +0500

    fix: use cookie jar to easier login

--- znum.js
+++ znum.js
@@ -1,29 +1,21 @@
-const { existsSync, createWriteStream } = require('node:fs');
-const fs = require('node:fs/promises');
-const { createHmac } = require('node:crypto');
-const { join } = require('node:path');
-const { setTimeout } = require('node:timers/promises');
-const { fetch } = require('undici');
-const { joinImages } = require('join-images');
-const prompt = require('prompt');
-const cliProgress = require('cli-progress');
-const PDFDocument = require('pdfkit');
+import { existsSync, createWriteStream } from 'node:fs';
+import { readFile, writeFile, mkdir, unlink, rm } from 'node:fs/promises';
+import { createHmac } from 'node:crypto';
+import { join } from 'node:path';
+import { setTimeout } from 'node:timers/promises';
 
-const COOKIES_PATH = join(process.cwd(), 'cookies.txt');
+import { gotScraping } from 'got-scraping';
+import { Cookie, CookieJar } from 'tough-cookie';
+import { joinImages } from 'join-images';
+import { SingleBar, Presets } from 'cli-progress';
+import PDFDocument from 'pdfkit';
+import prompt from 'prompt';
+
+const COOKIES_PATH = join(process.cwd(), 'cookies.json');
 const DOWNLOADS_DIR = join(process.cwd(), 'downloads');
 
-const commonHeaders = {
-  accept: 'application/xml, text/xml, */*; q=0.01',
-  'accept-language': 'ru-RU,ru;q=0.9,en-NL;q=0.8,en-US;q=0.7,en;q=0.6,vi;q=0.5',
-  'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"macOS"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-origin',
-  'user-agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-};
+const DEFAULT_URL = 'https://znanium.ru/';
+const cookieJar = new CookieJar();
 
 const getTextBetween = (source, startStr, endStr) => source?.split(startStr)?.[1]?.split(endStr)?.[0];
 
@@ -32,14 +24,10 @@ const getPageUrl = (contentId, pageNumber) =>
 
 function createToken(documentId, pageNumber, cryptoKey, cryptoKeyId, t) {
   const timestamp = Math.floor(Date.now() / 1000);
-  this.timeSyncDelta = 1;
-  this.secLog = `init(time:${timestamp},serverTime:${timestamp},key:${cryptoKey},id:${cryptoKeyId},${cryptoKey}:${cryptoKeyId});`;
-  if (this.lastJwt && this.lastJwtTime && timestamp - this.lastJwtTime < 90 && this.lastJwtCalls < 10) {
-    this.lastJwtCalls += 1;
-    return this.lastJwt;
-  }
+  const timeSyncDelta = 1;
+  const secLog = `init(time:${timestamp},serverTime:${timestamp},key:${cryptoKey},id:${cryptoKeyId},${cryptoKey}:${cryptoKeyId});`;
   const headerString = Buffer.from(JSON.stringify({ alg: 'HS256', typ: 'JWT' })).toString('base64');
-  const time = timestamp - this.timeSyncDelta;
+  const time = timestamp - timeSyncDelta;
   const key = cryptoKey || 'a1b2c3d4e5';
   const body = {
     id: 'ZNANIUM-JWT',
@@ -50,40 +38,35 @@ function createToken(documentId, pageNumber, cryptoKey, cryptoKeyId, t) {
     iat: time - 120,
     secid: cryptoKeyId,
     localTime: timestamp,
-    deltaTime: this.timeSyncDelta,
-    log: this.secLog,
+    deltaTime: timeSyncDelta,
+    log: secLog,
     case: t ? 1 : 0,
   };
   const bodyString = Buffer.from(JSON.stringify(body)).toString('base64');
   const hmac = createHmac('sha256', key);
   hmac.update(`${headerString}.${bodyString}`);
   const hashString = hmac.digest('base64');
-  this.lastJwtCalls = 1;
-  this.lastJwtTime = timestamp;
-  this.lastJwt = `${headerString}.${bodyString}.${hashString}`;
-  return this.lastJwt;
+  const lastJwt = `${headerString}.${bodyString}.${hashString}`;
+  return lastJwt;
 }
 
-const fetchDocumentInfo = async (contentUrl, cookie) => {
-  const response = await fetch(contentUrl, { headers: { cookie, ...commonHeaders } });
-  const body = await response.text();
-  const pagesAllText = getTextBetween(body, 'pages__all">', '<');
+const fetchDocumentInfo = async (contentUrl) => {
+  const response = await gotScraping(contentUrl, { cookieJar });
+  const pagesAllText = getTextBetween(response.body, 'pages__all">', '<');
   const pagesCount = pagesAllText.replace(/^\D+/g, '');
-  const keyString = getTextBetween(body, 'id="render-ver" type="hidden" value="', '"');
+  const keyString = getTextBetween(response.body, 'id="render-ver" type="hidden" value="', '"');
   const [cryptoKey, cryptoKeyId] = keyString.split(':');
   return { pagesCount: parseInt(pagesCount), cryptoKey, cryptoKeyId };
 };
 
-const fetchPage = async (contentId, pageNumber, cookie, token) => {
+const fetchPage = async (contentId, pageNumber, token) => {
   const pageUrl = getPageUrl(contentId, pageNumber);
-  const response = await fetch(pageUrl, { headers: { cookie, authorization: `Bearer ${token}`, ...commonHeaders } });
-  const body = await response.text();
-  // console.log(body);
-  const statusText = getTextBetween(body, '<status_text>', '</status_text>');
+  const response = await gotScraping(pageUrl, { headers: { authorization: `Bearer ${token}` }, cookieJar });
+  const statusText = getTextBetween(response.body, '<status_text>', '</status_text>');
   const slicesB64 = [];
   let currentSlice = 1;
   do {
-    const sliceFragment = getTextBetween(body, `<slice${currentSlice}>`, `</slice${currentSlice}>`);
+    const sliceFragment = getTextBetween(response.body, `<slice${currentSlice}>`, `</slice${currentSlice}>`);
     const sliceB64 = getTextBetween(sliceFragment, ',', ']');
     if (sliceB64) {
       slicesB64.push(sliceB64);
@@ -91,93 +74,105 @@ const fetchPage = async (contentId, pageNumber, cookie, token) => {
     } else currentSlice = -1;
   } while (currentSlice >= 1);
   const slices = slicesB64.map((data) => Buffer.from(data, 'base64'));
-  return { statusText, slices, body, headers: response.headers, statusCode: response.status };
+  return { statusText, slices, statusCode: response.statusCode };
 };
 
-// https://znanium.ru/read?id=420612
-
-const fetchToken = async () => {
-  const response = await fetch('https://znanium.ru/', { headers: commonHeaders });
-  const body = await response.text();
-  const csrfToken = getTextBetween(body, 'name="csrf-token" content="', '"');
-  return csrfToken;
+const fetchLoginPage = async () => {
+  const response = await gotScraping('https://znanium.ru/site/login', { cookieJar });
+  const csrfToken = getTextBetween(response.body, 'name="csrf-token" content="', '"');
+  return { csrfToken };
 };
 
-const fetchCookies = async (csrfToken, username, password) => {
-  const response = await fetch('https://znanium.ru/site/login', {
+const fetchAuthCookies = async (username, password, csrfToken) => {
+  await gotScraping('https://znanium.ru/site/login', {
     method: 'POST',
-    headers: { ...commonHeaders, 'Content-Type': 'application/x-www-form-urlencoded' },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    cookieJar,
     body: new URLSearchParams({
       '_csrf-frontend': csrfToken,
       'LoginForm[username]': username,
       'LoginForm[password]': password,
       'LoginForm[rememberMe]': '1',
-      'LoginForm[returnUrl]': 'https://znanium.ru/',
+      'LoginForm[returnUrl]': DEFAULT_URL,
       did: '',
       pid: '',
       page: '',
       'login-button': '',
-    }),
+    }).toString(),
   });
-  return response.headers.get('set-cookie');
+};
+
+const fetchMainCookies = async () => {
+  await gotScraping(DEFAULT_URL, { cookieJar });
 };
 
 const login = async (username, password) => {
-  if (existsSync(COOKIES_PATH)) return fs.readFile(COOKIES_PATH, 'utf-8').then((data) => data.split('\n').join(' '));
+  if (existsSync(COOKIES_PATH)) {
+    const data = await readFile(COOKIES_PATH, 'utf-8').then((data) => JSON.parse(data));
+    for (const cookie of data) await cookieJar.setCookie(Cookie.parse(cookie), DEFAULT_URL);
+    return;
+  }
   const answer = username && password ? { username, password } : await prompt.get(['username', 'password']);
   console.log('Авторизация...');
-  const csrfToken = await fetchToken();
-  const cookies = await fetchCookies(csrfToken, answer.username, answer.password);
-  await fs.writeFile(COOKIES_PATH, cookies);
+  const { csrfToken } = await fetchLoginPage();
+  console.log(cookieJar.toJSON());
+  await fetchAuthCookies(answer.username, answer.password, csrfToken);
+  console.log(cookieJar.toJSON());
+  await fetchMainCookies();
+  // console.log(cookieJar.toJSON());
+  const cookies = await cookieJar.getCookies(DEFAULT_URL).then((cookies) => cookies.map((cookie) => cookie.toString()));
+  await writeFile(COOKIES_PATH, JSON.stringify(cookies, null, 2));
   return cookies;
 };
 
 (async () => {
-  const cookies = await login('vitalygashkov@vk.com', '@THbyAKJ46jRU@');
+  await login();
 
-  const { url } = { url: 'https://znanium.ru/read?id=420612' } || (await prompt.get(['url']));
+  const { url } = await prompt.get(['url']);
   const documentId = getTextBetween(url, 'id=', '&');
 
   console.log('Получение информации о книге...');
-  const { pagesCount, cryptoKey, cryptoKeyId } = await fetchDocumentInfo(url, cookies);
+  const info = await fetchDocumentInfo(url);
 
   let error = '';
-  let currentPage = 1;
+  let currentPage = 11;
 
   const workDir = join(DOWNLOADS_DIR, documentId);
   const pages = [];
 
   console.log('Скачивание изображений...');
-  const downloadProgress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  downloadProgress.start(pagesCount - currentPage + 1, 0);
+  const downloadProgress = new SingleBar({}, Presets.shades_classic);
+  downloadProgress.start(info.pagesCount - currentPage + 1, 0);
   do {
-    const token = createToken(documentId, currentPage, cryptoKey, cryptoKeyId);
-    const { statusText, slices, statusCode } = await fetchPage(documentId, currentPage, cookies, token);
+    const token = createToken(documentId, currentPage, info.cryptoKey, info.cryptoKeyId);
+    const { statusText, slices, statusCode } = await fetchPage(documentId, currentPage, token);
     if (statusText !== 'OK') {
       error = statusText || statusCode;
       console.error(`\nСтраница ${currentPage}. Ошибка: ${statusText || statusCode}`);
     } else {
       const sliceNames = slices.map((_, i) => `page_${currentPage}_${i}.png`);
       const slicePaths = sliceNames.map((name) => join(workDir, name));
-      await fs.mkdir(workDir, { recursive: true });
-      for (let i = 0; i < slices.length; i++) await fs.writeFile(slicePaths[i], slices[i]);
+      await mkdir(workDir, { recursive: true });
+      for (let i = 0; i < slices.length; i++) await writeFile(slicePaths[i], slices[i]);
       const imageObject = await joinImages(slicePaths);
       const output = join(workDir, `page_${currentPage}.png`);
       imageObject.toFile(output);
-      for (let i = 0; i < slices.length; i++) await fs.unlink(slicePaths[i]);
+      for (let i = 0; i < slices.length; i++) await unlink(slicePaths[i]);
       pages.push(output);
       downloadProgress.update(currentPage);
     }
     await setTimeout(1000);
     currentPage++;
-  } while (!error && currentPage <= pagesCount);
+  } while (!error && currentPage <= info.pagesCount);
   downloadProgress.stop();
   if (error) return console.error('Ошибка скачивания');
   else console.log('Скачивание завершено');
 
   await setTimeout(500);
   console.log('Конвертирование изображений в PDF...');
-  const pdfProgress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  const pdfProgress = new SingleBar({}, Presets.shades_classic);
   pdfProgress.start(pages.length, 0);
   const pdfPath = join(DOWNLOADS_DIR, `${documentId}.pdf`);
   const doc = new PDFDocument({ autoFirstPage: false });
@@ -193,7 +188,7 @@ const login = async (username, password) => {
     doc.image(pageImage, 0, 0);
     pdfProgress.update(i);
   }
-  await fs.rm(workDir, { recursive: true, force: true });
+  await rm(workDir, { recursive: true, force: true });
   doc.end();
 
   pdfProgress.stop();
